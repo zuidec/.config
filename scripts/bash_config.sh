83ed1cd5125881a910a0cd7257bd7dd3b61bd4e8
zuidec <case@case-school>
2023-11-07 11:30:23 -0800

    Modified packer clone command and added commands to init git submodules

--- scripts/bash_config.sh
+++ scripts/bash_config.sh
@@ -1,1 +1,7 @@
+mv ~/.bashrc ~/.bashrc.old
 ln -s ~/.config/bash/.bashrc ~/.bashrc
+
+cd ~/.config
+
+git submodule init
+git submodule update
--- scripts/nvim_config.sh
+++ scripts/nvim_config.sh
@@ -1,1 +1,3 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
