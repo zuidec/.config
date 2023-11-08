f4f97dacb7a78501ed336dc99f3ff988515b2377
zuidec <case@CASE-DEKSTOP>
2023-11-08 00:48:31 -0800

    Added ssh_key_list creation in bash config script

--- scripts/bash_config.sh
+++ scripts/bash_config.sh
@@ -5,3 +5,9 @@ cd ~/.config
 
 git submodule init
 git submodule update
+
+touch ~/.ssh/ssh_key_list
+
+echo "Bash configuration complete, update ssh key list located at"
+echo "~/.ssh/ssh_key_list"
+echo ""
