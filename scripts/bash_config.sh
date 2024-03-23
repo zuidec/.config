2013566fdcf4b5d3ad7c407710be2260993f8c23
zuidec <case@CASE-DEKSTOP>
2024-03-22 21:45:52 -0700

    Fixed setup script

--- scripts/bash_config.sh
+++ scripts/bash_config.sh
@@ -3,9 +3,6 @@ ln -s ~/.config/bash/.bashrc ~/.bashrc
 
 cd ~/.config
 
-git submodule init
-git submodule update
-
 touch ~/.ssh/ssh_key_list
 
 echo "Bash configuration complete, update ssh key list located at"
